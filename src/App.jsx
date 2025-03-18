d2dd698c310816a6277bd80bca0118fb9e1ef785
Benjamin <goosta19802@gmail.com>
2025-03-18 20:40:35 -0300

    se corrige error nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Footer from './Components/Footer';
-import Navbar from './Components/NavBar';
+import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importar React Router
 
 function App() {
