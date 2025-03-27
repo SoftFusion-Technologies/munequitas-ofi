78ee04f5808a7aed283abd9dde1fee1f1ceb5725
Benjamin <goosta19802@gmail.com>
2025-03-27 17:12:24 -0300

    Listo largos

--- src/Helpers/Arrays/largos.js
+++ src/Helpers/Arrays/largos.js
@@ -1,6 +1,6 @@
-export const largos = Array.from({ length: 4 }, (_, i) => ({
+export const largos = Array.from({ length: 112 }, (_, i) => ({
   id: 101 + i,
   title: 'VESTIDO largos',
   categoria: 'simple',
-  imageFront: `/Largos/largos${i + 1}.jpg`
+  imageFront: `/Largos/largos${i + 1}.jpeg`
 }));
