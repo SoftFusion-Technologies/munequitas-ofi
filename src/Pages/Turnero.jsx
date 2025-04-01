7341d6bb6bd0efca3c09233cb57caaafef7e422e
Benjamin <goosta19802@gmail.com>
2025-04-01 00:09:58 -0300

    cambio cel

--- src/Pages/Turnero.jsx
+++ src/Pages/Turnero.jsx
@@ -43,7 +43,7 @@ const Turnero = () => {
     - Categoría: ${categoria}`;
 
     // Número de WhatsApp
-    const telefono = '3812062925';
+    const telefono = '+5493812062925';
 
     // Abrir WhatsApp en una nueva ventana
     window.open(`https://wa.me/${telefono}?text=${mensaje}`, '_blank');
